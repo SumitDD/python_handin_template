42c01982c526956d8fff6f8431a8bfd7d94d76da
Sumit Dey <sumitdey0007@gmail.com>
2021-02-28 17:15:15 +0100

    opgave 1, 6 rettet

--- modules/ex1_Week4.py
+++ modules/ex1_Week4.py
@@ -58,7 +58,7 @@ def above65years():
 
 
 def above65years_FromNordicCountries():
-    mask = ((bef_stats_df[:, 0] == 2015) & (bef_stats_df[:, 2] > 65)) & (bef_stats_df[:, 3] == 5101) or (
+    mask = (bef_stats_df[:, 0] == 2015) & (bef_stats_df[:, 2] > 65) & (bef_stats_df[:, 3] == 5101) or (
         bef_stats_df[:, 3] == 5104) or (bef_stats_df[:, 3] == 5110) or (bef_stats_df[:, 3] == 5120) or (bef_stats_df[:, 3] == 5106)
     print('sum of all the people above 65 years old and from nordic countries besides Denmark\n',
           np.sum(bef_stats_df[mask][:, 4]))
